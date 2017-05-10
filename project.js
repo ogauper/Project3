4c693bc05c53475d472acd1ef91d3a977a3f57f6
ogauper <ogauper@mail.csuchico.edu>
2017-05-09 21:56:29 -0700

    Update project.js
    final commit

--- project.js
+++ project.js
@@ -1,7 +1,9 @@
 //Orrin Gauper
 
+
 var count = 0;
 var Qarray = [];
+var current = -1;
 
 function question(qtext, array){
     this.quest = qtext;
@@ -10,31 +12,19 @@ function question(qtext, array){
 }
 
 function Submit(){
-  //var qtext = document.getElementById("Qtext").value;
-  //localStorage.qtext = document.getElementById("Qtext").value;
-  //setCookie("qtext", document.getElementById("Qtext").value, 100);
-  //setCookie("option1", document.getElementById("Op1").value, 100);
-  //setCookie("option2", document.getElementById("Op2").value, 100);
-  //setCookie("option3", document.getElementById("Op3").value, 100);
+  if (document.getElementById("ans").value == ''){
+    alert("You must enter an answer. ");
+    return;
+  }
+  var qtext = document.getElementById("Qtext").value;
   var option1 = document.getElementById("Op1").value;
   var option2 = document.getElementById("Op2").value;
   var option3 = document.getElementById("Op3").value;
   var answer = document.getElementById("ans").value;
-  var array = [option1, option2, option3, answer ];
-  //Qarray[count] = new question(qtext, array);
 
-  window.localStorage.setItem('qtext', document.getElementById("Qtext").value );
-  var obj =  window.localStorage.getItem('qtext');
+  var array = [option1, option2, option3, answer ];
+  Qarray[count] = new question(qtext, array);
   count = count + 1;
-  /*var h = document.createElement("li");
-  var p = document.createElement("p");
-  var t = document.createTextNode(Qarray[0].quest);
-  var list = document.getElementById("listDisplay");
-  h.appendChild(t);
-  list.appendChild(h);
-  var x = document.createTextNode("A. " + Qarray[0].answers[0]);
-  p.appendChild(x);
-  h.appendChild(p); */ 
 
   document.getElementById("Qtext").value = "";
   document.getElementById("Op1").value = "";
@@ -45,43 +35,81 @@ function Submit(){
 
 
 
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 
 function Done(){
-  //setCookie("Cookie", Qarray[0].quest, 100);
+  for(var i = 0; i < count; i++){
+    localStorage.setItem(('Q' + i) , Qarray[i].quest);
+    localStorage.getItem('Q' + i);
+      for(var j = 0; j < Qarray[i].answers.length; j++){
+        if(j == 3){
+          localStorage.setItem(('Q' + i + '_ans') , Qarray[i].answers[j]);}
+        else  {
+          localStorage.setItem(('Q' + i + '_op' + j) , Qarray[i].answers[j]); }
+          }
+      }
+  localStorage.setItem('count' , count);
   window.location = 'page2.html';
- //var make = localStorage.getItem(Qarray[0]);
 }
 
 function Load(){
-  //var make = getCookie("Cookie");
-  //var newarray = localStorage.getItem(Qarray[0]);
-    var h = document.createElement("li");
-  var p = document.createElement("p");
-  var t = document.createTextNode(localStorage.getItem('qtext'));
+
   var list = document.getElementById("listDisplay");
-  h.appendChild(t);
-  list.appendChild(h)
+  var count = localStorage.getItem('count');
+
+  for(var i = 0; i < count; i++){
+      var li = document.createElement("li");
+      var form = document.createElement("form");
+      var Qtext = document.createTextNode(localStorage.getItem('Q' + i));
+      li.appendChild(Qtext);//Appends Question text to li element
+      var ol = document.createElement("ol");
+      ol.setAttribute("type", "A");
+      li.appendChild(form);
+      form.appendChild(ol);
+      numArray = [0, 1, 2, 3];
+      for(var j = 0; j < 4; j++){
+        num = RandNUM(numArray);
+        var Qli = document.createElement("li");
+
+        var aPara = document.createElement("input");
+        aPara.setAttribute("type", "radio");
+        if(num == 3){
+            var label = document.createTextNode(localStorage.getItem('Q' + i + '_ans'));
+            aPara.setAttribute("name", 'ans' + i);
+            aPara.setAttribute("id" , 'ans' + i);
+            }
+        else{
+            var label = document.createTextNode(localStorage.getItem('Q' + i + '_op' + num));
+            aPara.setAttribute("name", "Q" + i);
+            aPara.setAttribute("id" , 'Q' + i + '_op' + num);
+          }
+
+        Qli.appendChild(label);
+        Qli.appendChild(aPara);
+        ol.appendChild(Qli);
+    }
+
+      list.appendChild(li);
+    }
+
 }
 
+function RandNUM(numArray){
+  if( numArray.length == 1 )
+      return numArray[0];
+var num = Math.floor(Math.random() * (numArray.length)  );
+var newnum = numArray[num];
+numArray.splice(num, 1);
+
+return newnum;}
+
+function CalcScore(){
+  var score = 0;
+  var count = localStorage.getItem('count');
+  for(var i = 0; i < count; i++){
+    if(document.getElementById('ans' + i).checked)
+      score = score + 1;
+  }
+  score = (score / count * 100).toFixed(2);
+  alert("Your score is: " + score + '%');
+}
